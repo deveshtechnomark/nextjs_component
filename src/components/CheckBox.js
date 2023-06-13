c7d61f5aeb95d25e832fac1f45a3f2d3b7001a34
nikhilnaagar <nnnagar1@gmail.com>
2023-06-13 13:48:33 +0530

    animation added

---
+++ src/components/CheckBox.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styles from "../scss/styles.module.scss";
+
+class CheckBox extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const { className, label, id, invalid, intermediate, checked, disabled } = this.props;
+        return (
+            <>
+                <input
+                    className={`${className} ${styles.checkBox} ${invalid && styles.checkBoxInvalid} `}
+                    type="checkbox"
+                    id={id}
+                    checked={checked}
+                    disabled={disabled}
+                />
+                <label className={`${styles.label}`} htmlFor={id}>
+                    <span
+                        className={`${styles.checkBoxDiv} ${intermediate && styles.intermediate
+                            }`}
+                    >
+                    </span>
+                    {label}
+                </label>
+            </>
+        );
+    }
+}
+
+export { CheckBox };
---
+++ src/index.js
@@ -0,0 +1,1 @@
+export {CheckBox} from "./components/CheckBox";
