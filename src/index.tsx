25ba2647ebdee70942848e674d6642bb52148755
HarshTechnomark <harsh.dhaduk@technomark.io>
2023-06-23 16:09:52 +0530

    removed hover animation from this code

--- src/index.tsx
+++ src/index.tsx
@@ -51,23 +51,24 @@ const Button: React.FC<ButtonProps> = ({
           style={style}
           className={`${className} button group ${
             variant === "btn" && "!bg-[#D8D8D8] !text-[#6E6D7A]"
-          } ${variant === "btn-primary" && "!bg-[#02B89D] !text-white"} ${
-            variant === "btn-secondary" && "!bg-[#069CDE] !text-white"
-          } ${variant === "btn-success" && "!bg-[#198754] !text-white"} ${
-            variant === "btn-error" && "!bg-[#DC3545] !text-white"
-          } ${variant === "btn-warning" && "!bg-[#FFC107] !text-white"} ${
-            variant === "primary" && "!text-[#02B89D]"
-          } ${variant === "secondary" && "!text-[#069CDE]"} ${
-            variant === "success" && "!text-[#198754]"
-          } ${variant === "error" && "!text-[#DC3545]"} ${
-            variant === "warning" && "!text-[#FFC107]"
-          } ${variant === "btn-outline" && "btn-outline"} ${
-            variant === "btn-outline-primary" && "btn-outline-primary"
-          } ${variant === "btn-outline-secondary" && "btn-outline-secondary"} ${
-            variant === "btn-outline-success" && "btn-outline-success"
-          } ${variant === "btn-outline-error" && "btn-outline-error"} ${
-            variant === "btn-outline-warning" && "btn-outline-warning"
-          }`}
+          } ${
+            variant === "btn-primary" &&
+            "!bg-[#02B89D] !text-white hover:!bg-[#029882]"
+          } ${variant === "btn-secondary" && "!bg-[#069CDE] !text-white hover:!bg-[#0281B9]"} ${
+            variant === "btn-success" && "!bg-[#198754] !text-white hover:!bg-[#146C43]"
+          } ${variant === "btn-error" && "!bg-[#DC3545] !text-white hover:!bg-[#B02A37]"} ${
+            variant === "btn-warning" && "!bg-[#FFC107] !text-white hover:!bg-[#CC9A06]"
+          } ${variant === "primary" && "!text-[#02B89D]"} ${
+            variant === "secondary" && "!text-[#069CDE]"
+          } ${variant === "success" && "!text-[#198754]"} ${
+            variant === "error" && "!text-[#DC3545]"
+          } ${variant === "warning" && "!text-[#FFC107]"} ${
+            variant === "btn-outline" && "btn-outline"
+          } ${variant === "btn-outline-primary" && "btn-outline-primary hover:!bg-[#E0F8F4]"} ${
+            variant === "btn-outline-secondary" && "btn-outline-secondary hover:!bg-[#E5F5FC]"
+          } ${variant === "btn-outline-success" && "btn-outline-success hover:!bg-[#D1E7DD]"} ${
+            variant === "btn-outline-error" && "btn-outline-error hover:!bg-[#F8D7DA]"
+          } ${variant === "btn-outline-warning" && "btn-outline-warning hover:!bg-[#FFF3CD]"}`}
           type={type}
           disabled={disabled}
           onBlur={onBlur}
@@ -76,32 +77,6 @@ const Button: React.FC<ButtonProps> = ({
           onSubmit={onSubmit}
           onClick={handleClick}
         >
-          <span
-            className={`absolute w-0 group-hover:w-[60%] transition-all duration-1000 ease-in-out h-full left-[50%] top-0 ${
-              variant === "btn-primary" && "!bg-[#029882]"
-            } ${variant === "btn-secondary" && "!bg-[#0281B9]"} ${
-              variant === "btn-success" && "!bg-[#146C43]"
-            } ${variant === "btn-error" && "!bg-[#B02A37]"} ${
-              variant === "btn-warning" && "!bg-[#CC9A06]"
-            } ${variant === "btn-outline-primary" && "!bg-[#E0F8F4]"} ${
-              variant === "btn-outline-secondary" && "!bg-[#E5F5FC]"
-            } ${variant === "btn-outline-success" && "!bg-[#D1E7DD]"} ${
-              variant === "btn-outline-error" && "!bg-[#F8D7DA]"
-            } ${variant === "btn-outline-warning" && "!bg-[#FFF3CD]"}`}
-          ></span>
-          <span
-            className={`absolute w-0 group-hover:w-[60%] transition-all duration-1000 ease-in-out h-full right-[50%] top-0 ${
-              variant === "btn-primary" && "!bg-[#029882]"
-            } ${variant === "btn-secondary" && "!bg-[#0281B9]"} ${
-              variant === "btn-success" && "!bg-[#146C43]"
-            } ${variant === "btn-error" && "!bg-[#B02A37]"} ${
-              variant === "btn-warning" && "!bg-[#CC9A06]"
-            } ${variant === "btn-outline-primary" && "!bg-[#E0F8F4]"} ${
-              variant === "btn-outline-secondary" && "!bg-[#E5F5FC]"
-            } ${variant === "btn-outline-success" && "!bg-[#D1E7DD]"} ${
-              variant === "btn-outline-error" && "!bg-[#F8D7DA]"
-            } ${variant === "btn-outline-warning" && "!bg-[#FFF3CD]"}`}
-          ></span>
           {ripples}
           <span className="relative" onClick={handleClick}>
             {children}
