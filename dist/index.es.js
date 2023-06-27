7a83569501812894b6afcc69d36aaf231966c4c3
HarshTechnomark <harsh.dhaduk@technomark.io>
2023-06-27 17:19:02 +0530

    toast completed with screen size

--- dist/index.es.js
+++ dist/index.es.js
@@ -38,25 +38,25 @@ var Toast = /** @class */function (_super) {
     return _super !== null && _super.apply(this, arguments) || this;
   }
   Toast.showToast = function (type, message, text, duration) {
-    var successIcon = "<svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"none\" viewBox=\"0 0 24 24\" stroke-width=\"1.5\" stroke=\"currentColor\" class=\"w-6 h-6\">\n    <path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z\" />\n  </svg>\n  ";
-    var errorIcon = "<svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"none\" viewBox=\"0 0 24 24\" stroke-width=\"1.5\" stroke=\"currentColor\" class=\"w-6 h-6\">\n    <path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z\" />\n  </svg>\n  ";
-    var warningIcon = "<svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"none\" viewBox=\"0 0 24 24\" stroke-width=\"1.5\" stroke=\"currentColor\" class=\"w-6 h-6\">\n    <path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z\" />\n  </svg>\n  ";
-    var infoIcon = "<svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"none\" viewBox=\"0 0 24 24\" stroke-width=\"1.5\" stroke=\"currentColor\" class=\"w-6 h-6\">\n  <path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z\" />\n</svg>\n";
+    var successIcon = "<svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"none\" viewBox=\"0 0 24 24\" stroke-width=\"1.5\" stroke=\"currentColor\" class=\"w-4 h-4 sm:w-6 sm:h-6\">\n    <path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z\" />\n  </svg>\n  ";
+    var errorIcon = "<svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"none\" viewBox=\"0 0 24 24\" stroke-width=\"1.5\" stroke=\"currentColor\" class=\"w-4 h-4 sm:w-6 sm:h-6\">\n    <path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z\" />\n  </svg>\n  ";
+    var warningIcon = "<svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"none\" viewBox=\"0 0 24 24\" stroke-width=\"1.5\" stroke=\"currentColor\" class=\"w-4 h-4 sm:w-6 sm:h-6\">\n    <path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z\" />\n  </svg>\n  ";
+    var infoIcon = "<svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"none\" viewBox=\"0 0 24 24\" stroke-width=\"1.5\" stroke=\"currentColor\" class=\"w-4 h-4 sm:w-6 sm:h-6\">\n  <path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z\" />\n</svg>\n";
     var toastElement = document.createElement("div");
-    toastElement.className = "flex flex-col text-base m-[6px] p-[12px] px-[16px] rounded-md text-white font-semibold min-w-15rem relative ".concat(type === "error" ? "bg-error !text-errorText" : type === "warning" ? "bg-warning !text-warningText" : type === "info" ? "bg-info !text-infoText" : "bg-success !text-successText");
+    toastElement.className = "flex flex-col text-xs sm:text-base m-[6px] p-[12px] px-[16px] rounded-md text-white font-semibold min-w-15rem relative ".concat(type === "error" ? "bg-error !text-errorText" : type === "warning" ? "bg-warning !text-warningText" : type === "info" ? "bg-info !text-infoText" : "bg-success !text-successText");
     var closeButton = document.createElement("span");
-    closeButton.className = "absolute top-1/4 right-[20px] w-15 h-15 bg-transparent border-none text-base cursor-pointer pl-10";
+    closeButton.className = "absolute top-1/4 right-[20px] w-15 h-15 bg-transparent border-none text-base cursor-pointer pl-10 text-xs sm:text-base";
     closeButton.innerHTML = "X";
     closeButton.addEventListener("click", function () {
       Toast.removeToast(toastElement);
     });
     var messageElement = document.createElement("span");
     messageElement.className = "flex flex-start items-center mr-20";
-    messageElement.innerHTML = "\n     <span>".concat(type === "error" ? errorIcon : type === "warning" ? warningIcon : type === "info" ? infoIcon : successIcon, "</span>\n     <Typography type=\"h6\"><span style=\"padding-left:5px\">").concat(message, "</span></Typography>");
+    messageElement.innerHTML = "\n     <span className=\"text-xs sm:text-base\">".concat(type === "error" ? errorIcon : type === "warning" ? warningIcon : type === "info" ? infoIcon : successIcon, "</span>\n     <Typography type=\"h6\"><span style=\"padding-left:5px\">").concat(message, "</span></Typography>");
     toastElement.appendChild(messageElement);
     toastElement.appendChild(closeButton);
     var textElement = document.createElement("span");
-    textElement.className = "flex flex-start items-center ml-[30px] text-sm font-normal";
+    textElement.className = "flex flex-start items-center ml-[22px] sm:ml-[30px] font-normal";
     textElement.innerHTML = "\n    <Typography type=\"label\"><span>".concat(text, "</span></Typography>");
     {
       text && toastElement.appendChild(textElement);
--- src/index.tsx
+++ src/index.tsx
@@ -22,25 +22,25 @@ class Toast extends React.Component<ToastProps> {
     text: string,
     duration?: number
   ) {
-    const successIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+    const successIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 sm:w-6 sm:h-6">
     <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
   </svg>
   `;
-    const errorIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+    const errorIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 sm:w-6 sm:h-6">
     <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z" />
   </svg>
   `;
-    const warningIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+    const warningIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 sm:w-6 sm:h-6">
     <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
   </svg>
   `;
-    const infoIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+    const infoIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 sm:w-6 sm:h-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
 </svg>
 `;
 
     const toastElement = document.createElement("div");
-    toastElement.className = `flex flex-col text-base m-[6px] p-[12px] px-[16px] rounded-md text-white font-semibold min-w-15rem relative ${
+    toastElement.className = `flex flex-col text-xs sm:text-base m-[6px] p-[12px] px-[16px] rounded-md text-white font-semibold min-w-15rem relative ${
       type === "error"
         ? "bg-error !text-errorText"
         : type === "warning"
@@ -52,7 +52,7 @@ class Toast extends React.Component<ToastProps> {
 
     const closeButton = document.createElement("span");
     closeButton.className =
-      "absolute top-1/4 right-[20px] w-15 h-15 bg-transparent border-none text-base cursor-pointer pl-10";
+      "absolute top-1/4 right-[20px] w-15 h-15 bg-transparent border-none text-base cursor-pointer pl-10 text-xs sm:text-base";
     closeButton.innerHTML = "X";
     closeButton.addEventListener("click", () => {
       Toast.removeToast(toastElement);
@@ -61,7 +61,7 @@ class Toast extends React.Component<ToastProps> {
     const messageElement = document.createElement("span");
     messageElement.className = "flex flex-start items-center mr-20";
     messageElement.innerHTML = `
-     <span>${
+     <span className="text-xs sm:text-base">${
        type === "error"
          ? errorIcon
          : type === "warning"
@@ -77,7 +77,7 @@ class Toast extends React.Component<ToastProps> {
 
     const textElement = document.createElement("span");
     textElement.className =
-      "flex flex-start items-center ml-[30px] text-sm font-normal";
+      "flex flex-start items-center ml-[22px] sm:ml-[30px] font-normal";
     textElement.innerHTML = `
     <Typography type="label"><span>${text}</span></Typography>`;
     {
