955e02c70cfcdd0cbc6c4ed4c0149f0f2e1fc8e2
HarshTechnomark <harsh.dhaduk@technomark.io>
2023-06-28 09:40:40 +0530

    Typography added into alert

--- dist/index.es.js
+++ dist/index.es.js
@@ -12,98 +12,84 @@ var Alert = function Alert(_a) {
     setVisible(false);
   };
   var alertClass = "";
-  var icon = null;
   switch (variant) {
     case "success":
       alertClass = "!bg-success !text-successText";
-      icon = React.createElement("svg", {
-        xmlns: "http://www.w3.org/2000/svg",
-        fill: "none",
-        viewBox: "0 0 24 24",
-        strokeWidth: "1.5",
-        stroke: "currentColor",
-        className: "w-6 h-6"
-      }, React.createElement("path", {
-        strokeLinecap: "round",
-        strokeLinejoin: "round",
-        d: "M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-      }));
       break;
     case "error":
       alertClass = "!bg-error !text-errorText";
-      icon = React.createElement("svg", {
-        xmlns: "http://www.w3.org/2000/svg",
-        fill: "none",
-        viewBox: "0 0 24 24",
-        strokeWidth: "1.5",
-        stroke: "currentColor",
-        className: "w-6 h-6"
-      }, React.createElement("path", {
-        strokeLinecap: "round",
-        strokeLinejoin: "round",
-        d: "M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"
-      }));
       break;
     case "warning":
       alertClass = "!bg-warning !text-warningText";
-      icon = React.createElement("svg", {
-        xmlns: "http://www.w3.org/2000/svg",
-        fill: "none",
-        viewBox: "0 0 24 24",
-        strokeWidth: "1.5",
-        stroke: "currentColor",
-        className: "w-6 h-6"
-      }, React.createElement("path", {
-        strokeLinecap: "round",
-        strokeLinejoin: "round",
-        d: "M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
-      }));
       break;
     case "info":
       alertClass = "!bg-info !text-infoText";
-      icon = React.createElement("svg", {
-        xmlns: "http://www.w3.org/2000/svg",
-        fill: "none",
-        viewBox: "0 0 24 24",
-        strokeWidth: "1.5",
-        stroke: "currentColor",
-        className: "w-6 h-6"
-      }, React.createElement("path", {
-        strokeLinecap: "round",
-        strokeLinejoin: "round",
-        d: "M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
-      }));
       break;
     default:
       alertClass = "!bg-success !text-successText";
-      icon = React.createElement("svg", {
-        xmlns: "http://www.w3.org/2000/svg",
-        fill: "none",
-        viewBox: "0 0 24 24",
-        strokeWidth: "1.5",
-        stroke: "currentColor",
-        className: "w-6 h-6"
-      }, React.createElement("path", {
-        strokeLinecap: "round",
-        strokeLinejoin: "round",
-        d: "M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-      }));
       break;
   }
-  return React.createElement(React.Fragment, null, React.createElement(Typography, {
-    type: "sub-title"
-  }, visible && React.createElement("div", {
+  var successIcon = React.createElement("svg", {
+    xmlns: "http://www.w3.org/2000/svg",
+    fill: "none",
+    viewBox: "0 0 24 24",
+    "stroke-width": "1.5",
+    stroke: "currentColor",
+    className: "w-4 h-4 sm:w-6 sm:h-6"
+  }, React.createElement("path", {
+    "stroke-linecap": "round",
+    "stroke-linejoin": "round",
+    d: "M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+  }));
+  var errorIcon = React.createElement("svg", {
+    xmlns: "http://www.w3.org/2000/svg",
+    fill: "none",
+    viewBox: "0 0 24 24",
+    "stroke-width": "1.5",
+    stroke: "currentColor",
+    className: "w-4 h-4 sm:w-6 sm:h-6"
+  }, React.createElement("path", {
+    "stroke-linecap": "round",
+    "stroke-linejoin": "round",
+    d: "M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"
+  }));
+  var warningIcon = React.createElement("svg", {
+    xmlns: "http://www.w3.org/2000/svg",
+    fill: "none",
+    viewBox: "0 0 24 24",
+    "stroke-width": "1.5",
+    stroke: "currentColor",
+    className: "w-4 h-4 sm:w-6 sm:h-6"
+  }, React.createElement("path", {
+    "stroke-linecap": "round",
+    "stroke-linejoin": "round",
+    d: "M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
+  }));
+  var infoIcon = React.createElement("svg", {
+    xmlns: "http://www.w3.org/2000/svg",
+    fill: "none",
+    viewBox: "0 0 24 24",
+    "stroke-width": "1.5",
+    stroke: "currentColor",
+    className: "w-4 h-4 sm:w-6 sm:h-6"
+  }, React.createElement("path", {
+    "stroke-linecap": "round",
+    "stroke-linejoin": "round",
+    d: "M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
+  }));
+  return React.createElement(React.Fragment, null, visible && React.createElement("div", {
     className: "flex flex-col m-[6px] p-[12px] px-[16px] rounded-md text-white min-w-15rem relative ".concat(alertClass),
     role: "alert"
   }, React.createElement("div", {
     className: "flex"
-  }, React.createElement("span", null, icon), React.createElement("span", {
-    className: "ml-2 mr-6"
+  }, React.createElement("span", null, variant === "error" ? errorIcon : variant === "warning" ? warningIcon : variant === "info" ? infoIcon : successIcon), React.createElement(Typography, {
+    type: "sub-title",
+    className: "ml-2 mr-6 text-xs sm:text-base"
   }, message)), React.createElement("button", {
     type: "button",
-    className: "absolute top-1/4 right-[20px] w-15 h-15 bg-transparent border-none text-base cursor-pointer pl-10",
+    className: "absolute top-1/4 right-[20px] w-15 h-15 bg-transparent border-none cursor-pointer pl-10 text-xs sm:text-base",
     onClick: handleClose
-  }, "X"))));
+  }, "X")));
 };
 
 export { Alert };
--- src/index.tsx
+++ src/index.tsx
@@ -16,128 +16,123 @@ const Alert: React.FC<AlertProps> = ({ variant, message }) => {
   };
 
   let alertClass = "";
-  let icon = null;
 
   switch (variant) {
     case "success":
       alertClass = "!bg-success !text-successText";
-      icon = (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-          />
-        </svg>
-      );
       break;
     case "error":
       alertClass = "!bg-error !text-errorText";
-      icon = (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"
-          />
-        </svg>
-      );
       break;
     case "warning":
       alertClass = "!bg-warning !text-warningText";
-      icon = (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
-          />
-        </svg>
-      );
       break;
     case "info":
       alertClass = "!bg-info !text-infoText";
-      icon = (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
-          />
-        </svg>
-      );
       break;
     default:
       alertClass = "!bg-success !text-successText";
-      icon = (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-          />
-        </svg>
-      );
       break;
   }
 
+  const successIcon = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      className="w-4 h-4 sm:w-6 sm:h-6"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+      />
+    </svg>
+  );
+  const errorIcon = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      className="w-4 h-4 sm:w-6 sm:h-6"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"
+      />
+    </svg>
+  );
+  const warningIcon = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      className="w-4 h-4 sm:w-6 sm:h-6"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
+      />
+    </svg>
+  );
+  const infoIcon = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      className="w-4 h-4 sm:w-6 sm:h-6"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
+      />
+    </svg>
+  );
+
   return (
     <>
-      <Typography type="sub-title">
-        {visible && (
-          <div
-            className={`flex flex-col m-[6px] p-[12px] px-[16px] rounded-md text-white min-w-15rem relative ${alertClass}`}
-            role="alert"
-          >
-            <div className="flex">
-              <span>{icon}</span>
-              <span className="ml-2 mr-6">{message}</span>
-            </div>
-            <button
-              type="button"
-              className="absolute top-1/4 right-[20px] w-15 h-15 bg-transparent border-none text-base cursor-pointer pl-10"
-              onClick={handleClose}
+      {visible && (
+        <div
+          className={`flex flex-col m-[6px] p-[12px] px-[16px] rounded-md text-white min-w-15rem relative ${alertClass}`}
+          role="alert"
+        >
+          <div className="flex">
+            <span>
+              {variant === "error"
+                ? errorIcon
+                : variant === "warning"
+                ? warningIcon
+                : variant === "info"
+                ? infoIcon
+                : successIcon}
+            </span>
+            <Typography
+              type="sub-title"
+              className="ml-2 mr-6 text-xs sm:text-base"
             >
-              X
-            </button>
+              {message}
+            </Typography>
           </div>
-        )}
-      </Typography>
+          <button
+            type="button"
+            className="absolute top-1/4 right-[20px] w-15 h-15 bg-transparent border-none cursor-pointer pl-10 text-xs sm:text-base"
+            onClick={handleClose}
+          >
+            X
+          </button>
+        </div>
+      )}
     </>
   );
 };
