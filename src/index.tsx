141d145b5e6f271ae471f67c604b875f75645941
Sandeep-52525 <sandeep.yadav@technomark.io>
2023-07-10 15:44:02 +0530

    TabBar library added!

--- src/index.tsx
+++ src/index.tsx
@@ -4,5 +4,6 @@ import Badge from "./components/Badge";
 import Radio from "./components/Radio";
 import CheckBox from "./components/CheckBox";
 import ProgressStep from "./components/ProgressStep";
+import TabBar from "./components/TabBar";
 
-export { CheckBox, Radio, Badge, ProgressStep };
+export { CheckBox, Radio, Badge, ProgressStep, TabBar };
