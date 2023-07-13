cb1363b1d226946665c33e1b0952ef5a3d049bae
deveshsheth <deveshssheth2018@gmail.com>
2023-07-13 10:29:17 +0530

    final commit success

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./style.css";
-interface ColorPickerProps {
-    onChange?: (value: string) => void;
-}
-export declare const ColorPicker: React.FC<ColorPickerProps>;
-export {};
