764ffeebcb67dffffa456b867963aec6a6fd34ab
HarshTechnomark <harsh.dhaduk@technomark.io>
2023-07-28 11:36:14 +0530

    updated code for validation

--- dist/index.js
+++ dist/index.js
@@ -877,7 +877,7 @@ var Tel = function Tel(_a) {
     id: id,
     name: name,
     value: value,
-    onBlur: onBlur ? onBlur : validate ? validateInput : focusHandler,
+    onBlur: onBlur ? onBlur : validate ? validateInput : validate ? focusHandler : undefined,
     onChange: handleInputChange,
     onFocus: handleFocus,
     disabled: disabled,
